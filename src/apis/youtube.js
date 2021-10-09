b2f07b9560f210133d9949366433a8146993d4dc
myildizCH <meyildiz.de@gmail.com>
2021-10-09 23:49:26 +0200

    update for implementing hooks

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "Youtube Key Here";
+const KEY = process.env.REACT_APP_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
